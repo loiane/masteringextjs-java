ac47356233706c791b9882eb15f82895b9b9b90d
Loiane Groner <loianeg@gmail.com>
2015-08-29 15:07:58 -0300

    spring security config

---
+++ src/main/java/com/loiane/mastering/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.loiane.mastering.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
