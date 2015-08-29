52179185eefe872124a7b9a77f75cba7e41d2388
Loiane Groner <loianeg@gmail.com>
2015-08-29 15:09:21 -0300

    spring mvc config

---
+++ src/main/java/com/loiane/mastering/configuration/SpringMvcInitializer.java
@@ -0,0 +1,23 @@
+package com.loiane.mastering.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
+
